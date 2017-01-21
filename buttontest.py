fe0fbb858642ea0ad11f4c5164f231f33aa8708f
Keller <keller@perka.com>
2017-01-21 10:47:27 -0500

    added v2 pinout led and button tests

---
+++ buttontest.py
@@ -0,0 +1,35 @@
+#gpio button test
+
+import RPi.GPIO as GPIO
+import time
+
+BUTTON0 = 16
+BUTTON1 = 18
+
+DEBOUNCE = 1
+
+#setup gpio
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(BUTTON0, GPIO.IN)
+GPIO.setup(BUTTON1, GPIO.IN)
+state0 = GPIO.input(BUTTON0)
+state1 = GPIO.input(BUTTON1)
+
+try:
+    print(BUTTON0)
+    print(BUTTON1)
+    #runloop
+    while True:
+        if GPIO.input(BUTTON0) != state0:
+            print("button 0 pressed")
+            time.sleep(DEBOUNCE)
+            state = GPIO.input(BUTTON0)
+        elif GPIO.input(BUTTON1) != state1:
+            print("button 1 pressed")
+            time.sleep(DEBOUNCE)
+            state = GPIO.input(BUTTON1)
+
+except KeyboardInterrupt:
+    GPIO.cleanup()
+        
+        
---
+++ ledtest.py
@@ -0,0 +1,38 @@
+#led test
+import RPi.GPIO as GPIO
+import time
+
+PIN0 = 29
+PIN1 = 31
+PIN2 = 33
+PIN3 = 35
+PIN4 = 37
+
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(PIN0, GPIO.OUT)
+GPIO.setup(PIN1, GPIO.OUT)
+GPIO.setup(PIN2, GPIO.OUT)
+GPIO.setup(PIN3, GPIO.OUT)
+GPIO.setup(PIN4, GPIO.OUT)
+
+try:
+    while True:
+        GPIO.output(PIN0, False)
+        GPIO.output(PIN1, False)
+        GPIO.output(PIN2, False)
+        GPIO.output(PIN3, False)
+        GPIO.output(PIN4, False)
+        time.sleep(0.5)	
+        GPIO.output(PIN0, True)
+        time.sleep(0.5)
+        GPIO.output(PIN1, True)
+        time.sleep(0.5)
+        GPIO.output(PIN2, True)
+        time.sleep(0.5)
+        GPIO.output(PIN3, True)
+        time.sleep(0.5)
+        GPIO.output(PIN4, True)
+        time.sleep(0.5)
+
+except KeyboardInterrupt:
+    GPIO.cleanup()
