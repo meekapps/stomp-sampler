3eb23d47c8dacce8ddb378dac4abe799b8935b38
Keller <keller@perka.com>
2017-02-05 16:00:51 -0500

    added launcher comments to start script on login

--- launcher.sh
+++ launcher.sh
@@ -1,6 +1,14 @@
 #!/bin/sh
 # launcher.sh
 # execute sampler_sampler.py on startup
+#
+# to install:
+# go to root directory: cd
+# create logs directory: mkdir logs
+# open crontab: sudo crontab -e
+# add script to crontab by adding the following line:
+# @reboot sh /home/pi/projects/stomp-sampler/launcher.sh >/home/pi/logs/cronlog 2>&1
+
 
 cd /home/pi/projects/stomp-sampler
 sudo python stomp_sampler.py
